6784895fd19dfde8b5339a49098ff735a26ff42f
Avaneesh-Pathak <avaneeshpathak900@gmail.com>
2023-05-16 19:40:44 +0530

    Diamond Price Prediction

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,39 +9,40 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionconfig:
-    train_data_path=os.path.join('artifacts','train.csv')
-    test_data_path=os.path.join('artifacts','test_csv')
-    raw_data_path=os.path.join('artifacts','raw_csv')
-    
-    
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test_csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw_csv')
+
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionconfig()
-        
+        self.ingestion_config = DataIngestionconfig()
+
     def initiate_data_ingestion(self):
         logging.info('Data Ingestion method start')
-        
-        try:
-            df=pd.reasd_csv(os.path.join('notebook','gemstone.csv'))
-            logging.info('Dataset read as pandas DataFrame')
-            
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False)
-            
-            
+        try:
+            df = pd.read_csv(os.path.join('notebook', 'gemstone.csv'))
+            logging.info('Dataset read as pandas DataFrame')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+
             logging.info("Train Test Split")
-            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
-            
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            
+            train_set, test_set = train_test_split(df, test_size=0.30, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
             logging.info("Ingestion of data is completed")
-            
-            return(
+
+            return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-            
+
         except Exception as e:
-            logging.info('Error occured in Data Ingestion config')
+            logging.info('Error occurred in Data Ingestion config')
+            logging.error(str(e))
+
+        return None
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,12 +1,32 @@
 import os
 import sys
+sys.path.append('D:\Diamond Price Prediction\src')
+
 from src.logger import logging
+
 from src.exception import CustomException
 import pandas as pd
-from src.components.data_ingestion import DataIngestion
+from src.components.data_ingestion import DataIngestion, DataIngestionconfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
 
 if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
+    obj = DataIngestion()
+    returned_value = obj.initiate_data_ingestion()
     
+    if returned_value is not None:
+        train_data_path, test_data_path = returned_value  # Unpack the returned value
+        print("Data ingestion successful.")
+        print("Train data path:", train_data_path)
+        print("Test data path:", test_data_path)
+    else:
+        # Handle the case when returned_value is None
+        print("Error: Data ingestion failed. Check the logs for more information.")
+        sys.exit(1)
+        
+    data_transformation=DataTransformation()
+    train__arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    
+    model_trainer = ModelTrainer()
+    model_trainer.initate_model_training(train__arr,test_arr)
